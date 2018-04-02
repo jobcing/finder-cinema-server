f07dbe3aef7e923c5ea4e568119c4df8e0b6b6f7
Cho ByeongChan <jobc0335@gmail.com>
2018-04-02 19:55:12 +0900

    init repository and add library

---
+++ src/main/java/com/fc/api/FinderCinemaServerApplication.java
@@ -0,0 +1,12 @@
+package com.fc.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinderCinemaServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinderCinemaServerApplication.class, args);
+	}
+}
