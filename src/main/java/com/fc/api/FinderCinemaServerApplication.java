db432f9582c529869579a6c9c07bc9b7297ab413
Cho ByeongChan <jobc0335@gmail.com>
2018-04-02 20:03:16 +0900

    development environment setup

--- src/main/java/com/fc/api/FinderCinemaServerApplication.java
+++ src/main/java/com/fc/api/FinderCinemaServerApplication.java
@@ -1,10 +1,21 @@
 package com.fc.api;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.cache.annotation.EnableCaching;
 
+@EnableAutoConfiguration
 @SpringBootApplication
-public class FinderCinemaServerApplication {
+@EnableCaching
+public class FinderCinemaServerApplication extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(FinderCinemaServerApplication.class);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(FinderCinemaServerApplication.class, args);
