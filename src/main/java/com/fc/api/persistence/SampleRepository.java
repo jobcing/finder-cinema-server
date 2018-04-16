8e490e54f2ef33a911f0528ef221b8853e373d23
Cho ByeongChan <jobc0335@gmail.com>
2018-04-16 19:37:22 +0900

    add sample repository

---
+++ src/main/java/com/fc/api/persistence/SampleRepository.java
@@ -0,0 +1,14 @@
+package com.fc.api.persistence;
+
+import com.fc.api.domain.SampleVO;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+/**
+ * Created by ByeongChan on 2018. 1. 15..
+ */
+
+@Repository
+public interface SampleRepository extends JpaRepository<SampleVO, Long> {
+    SampleVO findByVal1(String val1);
+}
